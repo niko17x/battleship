abd84bef48359cf5cafb5fd80a68f674a90812ea
Niko Shin <nshin17@gmail.com>
2022-12-11 08:31:17 -0800

    Removed unneccessary methods in Ship class.

--- src/factories/ships.js
+++ src/factories/ships.js
@@ -1,9 +1,62 @@
+// Helper Functions:
+// Functions and helpers:
+const helperPlaceShip = (values, coord) => {
+  if (values.includes(coord)) {
+    return true;
+  }
+  return false;
+};
+
+// Returns the index position of object x value:
+const getIndexPos = (coord, arr) => {
+  const xValue = coord;
+  return arr.indexOf(xValue);
+};
+
+// Creates instance of ship:
 class Ship {
   constructor(shipClass, shipLength, hitCount) {
     this.shipClass = shipClass;
     this.shipLength = shipLength;
     this.hitCount = hitCount;
+    this.coord = [];
     this.sunk = false;
+  }
+
+  updateCoord(x, y) {
+    const xInBound = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    const yInBound = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const xValues = helperPlaceShip(xInBound, x);
+    const yValues = helperPlaceShip(yInBound, y);
+    const shipOrient = "horizontal";
+
+    // Validate user coord input is within range.
+    if (xValues && yValues) {
+      const firstCoord = { x, y }; // User input coordinates.
+      this.coord.push(firstCoord);
+      const alphaIndex = getIndexPos(firstCoord.x, xInBound); // Get index pos. of first coord x value.
+      const numIndex = getIndexPos(firstCoord.y, yInBound); // Get index pos. of y value.
+
+      if (shipOrient === "vertical") {
+        // Vertical ship placement:
+        for (let i = 1; i < this.shipLength; i++) {
+          if (!this.coord[this.coord.length - 1].x) {
+            this.coord = [];
+            return;
+          }
+          this.coord.push({ x: xInBound[alphaIndex + i], y });
+        }
+      } else if (shipOrient === "horizontal") {
+        // Horizontal placement:
+        for (let i = 1; i < this.shipLength; i++) {
+          if (!this.coord[this.coord.length - 1].y) {
+            this.coord = [];
+            return;
+          }
+          this.coord.push({ x, y: yInBound[numIndex + i] });
+        }
+      }
+    }
   }
 
   // Increases the number of 'hits' in your ship:
@@ -20,6 +73,12 @@ class Ship {
     this.sunk = false;
     return false;
   }
+
+  getCoord() {
+    this.coord.placeCoord();
+  }
 }
 
 module.exports = Ship;
+
+// ! TEST AREA:
