d1a83889c6b3466eca7e7b52b34d92398dc2918c
Niko Shin <nshin17@gmail.com>
2023-01-07 22:13:36 -0800

    Cleaned up code.

--- src/factories/player.js
+++ src/factories/player.js
@@ -19,20 +19,20 @@ class Player {
 
   // Add default ships for start of game:
   defaultShips() {
-    // const carrier = new Ship("Carrier", 5, 0);
-    // const battleShip = new Ship("Battle-Ship", 4, 0);
-    // const destroyer = new Ship("Destroyer", 3, 0);
-    // const submarine1 = new Ship("Submarine-1", 2, 0);
-    // const submarine2 = new Ship("Submarine-2", 2, 0);
-    // const patrolBoat1 = new Ship("Patrol-Boat-1", 1, 0);
+    const carrier = new Ship("Carrier", 5, 0);
+    const battleShip = new Ship("Battle-Ship", 4, 0);
+    const destroyer = new Ship("Destroyer", 3, 0);
+    const submarine1 = new Ship("Submarine-1", 2, 0);
+    const submarine2 = new Ship("Submarine-2", 2, 0);
+    const patrolBoat1 = new Ship("Patrol-Boat-1", 1, 0);
     const patrolBoat2 = new Ship("Patrol-Boat-2", 1, 0);
     this.ships.push(
-      // carrier,
-      // battleShip,
-      // destroyer,
-      // submarine1,
-      // submarine2,
-      // patrolBoat1,
+      carrier,
+      battleShip,
+      destroyer,
+      submarine1,
+      submarine2,
+      patrolBoat1,
       patrolBoat2
     );
   }
